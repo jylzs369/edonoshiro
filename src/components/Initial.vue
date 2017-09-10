3f596618d8494b400d1c489b4c06be334532488d
Ushio <jylzs369@163.com>
2017-09-10 12:41:36 +0800

    增加开始画面初始化玩家窗口和读取存档窗口

---
+++ src/components/Initial.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="initial">
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
---
+++ src/components/Load.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="load">
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
