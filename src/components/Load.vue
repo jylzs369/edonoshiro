e3e445afad5f2579bc6fe450b8102555a791efa0
Ushio <jylzs369@163.com>
2017-09-28 10:13:39 +0800

    modify Load.vue

--- src/components/Load.vue
+++ src/components/Load.vue
@@ -1,10 +1,25 @@
 <template>
   <div class="load">
-    <ul class="record-list">
-      <li @click="" v-for="record in records">
-        <h5>title</h5>
-        <span>time</span>
-        <p>content</p>
+    <h2>
+      <button class="arrow-left"></button>
+      存档<span>1/2</span>
+      <button class="arrow-right"></button>
+    </h2>
+    <p>读取存档</p>
+    <ul class="record-list" v-for="record in records">
+      <li v-if="record.title" @click="">
+        <h5><i class="icon icon-auto-save"></i>title</h5>
+        <p><span class="fl">time</span><span class="fr">money</span></p>
+      </li>
+      <li v-else>
+        <h5><i class="icon icon-auto-save"></i>无存档</h5>
+      </li>
+      <li v-if="record.title" @click="">
+        <h5><i class="icon icon-save"></i>title</h5>
+        <p><span class="fl">time</span><span class="fr">money</span></p>
+      </li>
+      <li v-else @click="newRecord">
+        <h5><i class="icon icon-save"></i>空白存档</h5>
       </li>
     </ul>
   </div>
@@ -15,6 +30,10 @@ export default {
   data () {
     return {
       records: []
+    }
+  },
+  methords: {
+    newRecord () {
     }
   }
 }
