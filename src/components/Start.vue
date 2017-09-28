68eda1ac9edb19ec0ba463d2f3b0ebb240502db2
Ushio <jylzs369@163.com>
2017-09-28 10:23:53 +0800

    modify Start.vue

--- src/components/Start.vue
+++ src/components/Start.vue
@@ -1,13 +1,35 @@
 <template>
   <div class="start">
+    <header></header>
+    <div class="menu">
+      <button @click="load">开始</button>
+      <!-- <button>高分榜</button>-->
+    </div>
+    <load v-show="loadModal"></load>
+    <initial v-show="initialModal"></initial>
+    <footer></footer>
   </div>
 </template>
 
 <script>
+import Initial from '@/components/Initial.vue' 
+import Load from '@/components/Load.vue'
+
 export default {
   name: 'start',
+  components: {
+    Initial,
+    Load
+  },
   data () {
     return {
+      loadModal: false,
+      initialModal: false
+    }
+  },
+  methods: {
+    load () {
+      this.loadModal = true
     }
   }
 }
