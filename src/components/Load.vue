cd310000c8c5c0b48dda3a2efdcaddb3ce8bce8f
Ushio <jylzs369@163.com>
2017-10-24 14:08:39 +0800

    modify Load.vue

--- src/components/Load.vue
+++ src/components/Load.vue
@@ -1,42 +1,50 @@
 <template>
   <div class="load">
-    <h2>
-      <button class="arrow-left"></button>
-      存档<span>1/2</span>
-      <button class="arrow-right"></button>
-    </h2>
-    <p class="color-blue">读取存档</p>
-    <ul class="record-list" v-for="(record, index) in records" :key="index">
-      <li v-if="record.title" @click="load">
-        <h5>
-          <i class="icon icon-auto-save"></i>
-          <span>{{ record.title }}</span>
-        </h5>
-        <p><span class="fl">{{ record.time }}</span><span class="fr">{{ record.money }}</span></p>
-      </li>
-      <li v-else @click="newRecord">
-        <h5><i class="icon icon-auto-save"></i>无存档</h5>
-      </li>
-      <li v-if="record.title" @click="load">
-        <h5>
-          <i class="icon icon-save"></i>
-          <span>{{ record.title }}</span>
-        </h5>
-        <p><span class="fl">{{ record.time }}</span><span class="fr">{{ record.money }}</span></p>
-      </li>
-      <li v-else @click="newRecord">
-        <h5><i class="icon icon-save"></i>空白存档</h5>
-      </li>
-    </ul>
-    <p class="color-green">请选择</p>
+    <tab :title="tab.title" :page="tab.page" :totalTab="tab.totalTab" @leftTabClick="leftTabClick" @rightTabClick="rightTabClick"></tab>
+    <div class="tab-content">
+        <p class="color-blue">读取存档</p>
+        <ul class="record-list" v-for="(record, index) in records" :key="index" v-show="currentRecord - 1 === index">
+          <li v-if="record.title" @click="load">
+            <h5>
+              <i class="icon icon-auto-save"></i>
+              <span>{{ record.title }}</span>
+            </h5>
+            <p><span class="fl">{{ record.time }}</span><span class="fr">{{ record.money }}</span></p>
+          </li>
+          <li v-else @click="newRecord">
+            <h5><i class="icon icon-auto-save"></i>无存档</h5>
+          </li>
+          <li v-if="record.title" @click="load">
+            <h5>
+              <i class="icon icon-save"></i>
+              <span>{{ record.title }}</span>
+            </h5>
+            <p><span class="fl">{{ record.time }}</span><span class="fr">{{ record.money }}</span></p>
+          </li>
+          <li v-else @click="newRecord">
+            <h5><i class="icon icon-save"></i>空白存档</h5>
+          </li>
+        </ul>
+        <p class="color-green">请选择</p>
+    </div>
   </div>
 </template>
 
 <script>
+import Tab from '@/components/Tab.vue'
 export default {
+  components: {
+    Tab
+  },
   data () {
     return {
-      records: []
+      records: [],
+      currentRecord: 1,
+      tab: {
+        title: '存档',
+        page: true,
+        totalTab: 1
+      }
     }
   },
   methords: {
@@ -47,6 +55,10 @@ export default {
       this.$router.replace({
         name: 'Main'
       })
+    },
+    leftTabClick () {
+    },
+    rightTabClick () {
     }
   },
   created () {
@@ -55,8 +67,14 @@ export default {
         title: '江户城大院',
         time: '16年 4月1周',
         money: '1,300两'
+      },
+      {
+        title: '',
+        time: '',
+        money: ''
       }
     ]
+    this.tab.totalTab = parseInt(this.records.length)
   }
 }
 </script>
