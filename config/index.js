0ba6b18998718a4ce4dab1d4ad7327b134dfb1c1
Ushio <jylzs369@163.com>
2018-05-22 14:49:17 +0800

    modify index.js

--- config/index.js
+++ config/index.js
@@ -22,6 +22,7 @@ module.exports = {
     bundleAnalyzerReport: process.env.npm_config_report
   },
   dev: {
+    devtool: 'source-map',
     env: require('./dev.env'),
     port: 8999,
     autoOpenBrowser: true,
